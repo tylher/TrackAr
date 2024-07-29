75da046c669e0da5406ed122797dc5a598a32adb
Damola <tylher123@gmail.com>
2024-07-29 19:00:33 +0100

    add vercel.json

--- src/context/websocket-context.jsx
+++ src/context/websocket-context.jsx
@@ -4,11 +4,21 @@ import SockJS from "sockjs-client/dist/sockjs";
 
 const WebSocketContext = createContext();
 
-export const WebSocketProvider = ({ children }) => {
+const WebSocketProvider = ({ children }) => {
   const [stompClient, setStompClient] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
+
+  const sockJsOptions = {
+    transports: ["websocket", "xhr-streaming", "xhr-polling"],
+    timeout: 50000, // Custom timeout setting, in milliseconds
+    heartbeat: 25000, // Custom heartbeat interval, in milliseconds
+  };
   useEffect(() => {
-    const socket = new SockJS("http://localhost:8080/ws");
+    const socket = new SockJS(
+      "https://damoladev.uc.r.appspot.com/ws",
+      null,
+      sockJsOptions
+    );
     const client = Stomp.over(socket);
     client.connect(
       {},
@@ -34,4 +44,6 @@ export const WebSocketProvider = ({ children }) => {
   );
 };
 
-export const useWebSocket = () => useContext(WebSocketContext);
+const useWebSocket = () => useContext(WebSocketContext);
+
+export { WebSocketProvider, useWebSocket };
--- src/services/api/axiosInstance.js
+++ src/services/api/axiosInstance.js
@@ -3,7 +3,7 @@ import { BASE_URL } from "./endpoints";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL, // Replace with your API base URL
-  timeout: 10000, // Optional: Set a timeout for requests (in milliseconds)
+  // timeout: 10000, // Optional: Set a timeout for requests (in milliseconds)
   headers: {
     "Content-Type": "application/json",
     // Add any other default headers here
